d08fd1ec5f5df99be43f8956e50412af790c20bc
jeanvga <jeanvga@hotmail.com>
2019-11-05 19:56:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar backgroundColor="#f5f5f5" barStyle="dark-content" />
+      <Routes /> 
+    </>
+  );
+}
+
+
